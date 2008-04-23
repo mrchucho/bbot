4ad0d109353b1ec51ec7b174a4f1e4495ac64d36
mrchucho <mrchucho@mrchucho.net>
2008-04-23 18:50:39 +0000

    * Don't observe button unless it exists

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,7 @@
-$('preview_button').observe('click',function(event) {
-    $('comment_preview_author').innerHTML = $F('comment_author');
-    $('comment_preview_body').innerHTML = (superTextile($F('comment_body_raw')));
-    $('preview').show();
-})
+if (preview_button = $('preview_button')) {
+    preview_button.observe('click',function(event) {
+        $('comment_preview_author').innerHTML = $F('comment_author');
+        $('comment_preview_body').innerHTML = (superTextile($F('comment_body_raw')));
+        $('preview').show();
+    })
+}
