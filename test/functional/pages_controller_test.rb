a92eaa1e4e5dbd0f0362da3c8a57bb07fc29630c
mrchucho <mrchucho@mrchucho.net>
2009-10-09 13:25:05 -0500

    Updated PagesController functional test

--- test/functional/pages_controller_test.rb
+++ test/functional/pages_controller_test.rb
@@ -1,5 +1,9 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
+class PagesController
+  def rescue_action(e) raise e end
+end
+
 class PagesControllerTest < ActionController::TestCase
   def test_should_get_index
     get :index
@@ -8,36 +12,41 @@ class PagesControllerTest < ActionController::TestCase
   end
 
   def test_should_get_new
+    login_as(:quentin)
     get :new
     assert_response :success
   end
 
   def test_should_create_page
+    login_as(:quentin)
     assert_difference('Page.count') do
-      post :create, :page => { }
+      post :create, :page => {:title => "The Title"}
     end
 
-    assert_redirected_to page_path(assigns(:page))
+    assert_redirected_to page_path(assigns(:page).permalink)
   end
 
   def test_should_show_page
-    get :show, :id => pages(:one).id
+    get :show, :id => pages(:about).title
     assert_response :success
   end
 
   def test_should_get_edit
-    get :edit, :id => pages(:one).id
+    login_as(:quentin)
+    get :edit, :id => pages(:about).title
     assert_response :success
   end
 
   def test_should_update_page
-    put :update, :id => pages(:one).id, :page => { }
-    assert_redirected_to page_path(assigns(:page))
+    login_as(:quentin)
+    put :update, :id => pages(:about).title, :page => {:title => "New Title"}
+    assert_redirected_to page_path(assigns(:page).permalink)
   end
 
   def test_should_destroy_page
+    login_as(:quentin)
     assert_difference('Page.count', -1) do
-      delete :destroy, :id => pages(:one).id
+      delete :destroy, :id => pages(:about).title
     end
 
     assert_redirected_to pages_path
