3f7bc5acc5cac3da35af4e6687091c81e45b3c9a
mrchucho <mrchucho@mrchucho.net>
2009-10-09 11:59:36 -0500

    Updated restful_auth tests for Rails 2.3

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -4,12 +4,10 @@ require 'sessions_controller'
 # Re-raise errors caught by the controller.
 class SessionsController; def rescue_action(e) raise e end; end
 
-class SessionsControllerTest < Test::Unit::TestCase
+class SessionsControllerTest < ActionController::TestCase
   # Be sure to include AuthenticatedTestHelper in test/test_helper.rb instead
   # Then, you can remove it from this and the units test.
   include AuthenticatedTestHelper
-
-  fixtures :users
 
   def setup
     @controller = SessionsController.new
